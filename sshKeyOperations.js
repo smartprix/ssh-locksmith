57b4668e92d45c2d70bbe9eed6afaf12ca2725ee
vishal-smpx <vishal.sood@smartprix.com>
2018-09-11 19:28:06 +0530

    updated sshKeyOperations to console statements regarding progress + some other fixes

--- sshKeyOperations.js
+++ sshKeyOperations.js
@@ -1,4 +1,4 @@
-const {File, System, cfg} = require('sm-utils');
+const {File, System, cfg, Vachan} = require('sm-utils');
 const fs = require('fs'); // required for reading directory
 const nodemailer = require('nodemailer');
 
@@ -10,50 +10,56 @@ const transporter = nodemailer.createTransport({
 	path: '/usr/sbin/sendmail',
 });
 
-function sendMail(email, user, pathToPrivateKey) {
-    transporter.sendMail({
+async function sendMail(email, user, pathToPrivateKey) {
+    return transporter.sendMail({
         from: 'admin@smartprix.com',
         to: email,
-        subject: `SSH Key to Login as ${user} user on the server`,
-        text: `This mail has your private SSH key attached with it. You can use it to login to the server as "${user}".\n\nThe key is meant to be used by you and only you. Sharing or misusing of this key is strictly prohibited.`,
+        subject: `SSH Key to Login as ${user} user on ${cfg('hostname')}`,
+        text: `This mail has your private SSH key attached with it. You can use it to login to ${cfg('hostname')} as "${user}".\n\nThe key is meant to be used by you and only you. Sharing or misusing of this key is strictly prohibited.`,
         attachments: [
             {
                 path: pathToPrivateKey,
             },
         ],
-    }, (err, info) => {
-        // console.log(info.envelope);
-        // console.log(info.messageId);
     });
 }
 
 async function addUserKeyFor(user, email) {
+    const keyName = `${cfg('hostname', 'server')}_${user}`;
+    await new File(`./${keyName}.pub`).rmrf();
+    await new File(`./${keyName}`).rmrf();
+
     // generate key pair
-    System.exec(`ssh-keygen -t rsa -f newKey -C "${email}"`);
+    await System.exec(`ssh-keygen -t rsa -f ${keyName} -C "${email}" -N ""`);
+    console.log('New key pair generated');
 
     // copy public key to authorized_keys for 
     const file = new File(`${authorizedKeysDir}/${user}`);
-    const pubKey = new File('./newKey.pub');
-    const priKey = new File('./newKey');
+    const pubKey = new File(`./${keyName}.pub`);
+    const priKey = new File(`./${keyName}`);
 
     await file.append(await pubKey.read());
+    console.log('Added key for user', user);
     await pubKey.rm();
 
     // send private key to user via email
-    sendMail(email, user, priKey.realpathSync);
+    await sendMail(email, user, priKey.realpathSync());
+    console.log('Sent mail to email', email);
     await priKey.rm();
 }
 
-function deleteUserKeyFor(user, email) {
+async function deleteUserKeyFor(user, email) {
     // delete authorized_keys entry in the given user for the given email
-    System.exec(`sed -i '/${email}/d' ${authorizedKeysDir}/${user}`);
+    await System.exec(`sed -i '/${email}/d' ${authorizedKeysDir}/${user}`);
+    console.log(`Deleted keys for ${user} associated with ${email}`)
 }
 
-function deleteAllKeysFor(email) {
+async function deleteAllKeysFor(email) {
     // delete authorized_keys entries in all users for the given email
     const existingUsers = fs.readdirSync(authorizedKeysDir)
 
-    existingUsers.forEach((user) => deleteUserKeyFor(user, email));
+    return Vachan.promiseMap(existingUsers, async (user) => deleteUserKeyFor(user, email));
+    console.log(`Deleted all keys associated with ${email}`)
 }
 
 module.exports = {
