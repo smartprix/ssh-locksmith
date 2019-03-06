0c8b7866817a40cb691abbd9e84ed326e1221aee
Rohit Gohri <rohit.gohri@smartprix.com>
2019-03-06 16:58:52 +0530

    overwrite config

--- bin/ssh-locksmith.js
+++ bin/ssh-locksmith.js
@@ -3,7 +3,16 @@ const commandLineCommands = require('command-line-commands');
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
 
-cfg.file(`${__dirname}/config.js`);
+const {version} = require('../package.json');
+
+const confFile = process.env.SSH_LOCKSMITH_CONFIG;
+
+cfg.file(`${__dirname}/../config.js`, {overwrite: true});
+cfg.file(`${__dirname}/../private/config.js`, {ignoreNotFound: true});
+
+if (confFile) {
+	cfg.file(confFile, {ignoreNotFound: true});
+}
 
 const operations = require('../index');
 
@@ -14,15 +23,15 @@ const optionDefinitions = [
 	{
 		name: 'user',
 		type: String,
-        description: 'The remote user for which authorized key needs to be added.',
-        group: ['add', 'del'],
+		description: 'The remote user for which authorized key needs to be added.',
+		group: ['add', 'del'],
 	}, {
 		name: 'email',
 		type: String,
 		description: 'The email address which will be/was used as comment in the SSH key entry in the authorized keys. In case the command is {bold add}, the private key will also be sent to this email address.',
 		group: ['add', 'del'],
 	}, {
-        name: 'all',
+		name: 'all',
 		type: Boolean,
 		defaultValue: false,
 		description: 'If this option is set, then SSH keys for all users on the server related to {italic EMAIL} will be deleted.',
@@ -34,6 +43,10 @@ const sections = [
 	{
 		header: 'SSH Locksmith',
 		content: 'A node utility to manage {bold SSH keys} for different users on the server',
+	},
+	{
+		header: 'version',
+		content: `v${version}`,
 	},
 	{
 		header: 'Synopsis',
@@ -70,25 +83,25 @@ async function performCommand(com) {
 	try {
 		switch (com) {
 			case 'add':
-                options = options.add;
-                if (!options.user || !options.email) {
-                    console.error('Email/User not provided (required)');
-                }
-                else {
-                    await operations.addUserKeyFor(options.user, options.email)
-                }
+				options = options.add;
+				if (!options.user || !options.email) {
+					console.error('Email/User not provided (required)');
+				}
+				else {
+					await operations.addUserKeyFor(options.user, options.email)
+				}
 				break;
 
 			case 'del':
-                options = options.del;
-                if (!options.email) {
-                    console.error('Email not provided (required)');
-                }
-                else if (options.all) await operations.deleteAllKeysFor(options.email);
-                else {
-                    if (!options.user) console.error('User not provided (required)');
-                    else await operations.deleteUserKeyFor(options.user, options.email);
-                }
+				options = options.del;
+				if (!options.email) {
+					console.error('Email not provided (required)');
+				}
+				else if (options.all) await operations.deleteAllKeysFor(options.email);
+				else {
+					if (!options.user) console.error('User not provided (required)');
+					else await operations.deleteUserKeyFor(options.user, options.email);
+				}
 				break;
 
 			default:
